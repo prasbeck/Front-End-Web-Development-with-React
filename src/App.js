456a95fd801841ca8557491867e73df9ba9c3ca1
prasbeck <ch20b083@smail.iitm.ac.in>
2022-09-20 01:31:57 +0530

    Components part-1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { Navbar, NavbarBrand } from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
+import Menu from './components/MenuComponent';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          <NavbarBrand href="/">Ristorante ConFusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
