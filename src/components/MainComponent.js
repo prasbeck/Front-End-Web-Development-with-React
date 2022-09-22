2fe31433ad228bb78e1aa1cbba009f876efdaa47
prasbeck <ch20b083@smail.iitm.ac.in>
2022-09-22 05:34:31 +0530

    Single Page Applications Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -28,7 +28,14 @@ class Main extends Component {
   onDishSelect(dishId) {
     this.setState({ selectedDish: dishId});
   }
+  
   render(){
+    const DishWithId = ({match}) => {
+      return(
+          <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
     const HomePage = () => {
       return (
         <Home
@@ -44,6 +51,7 @@ class Main extends Component {
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Route path='/menu/:dishId' component={DishWithId} />
           <Route exact path='/contactus' component={Contact} />
           <Redirect to="/home" />
         </Switch>
