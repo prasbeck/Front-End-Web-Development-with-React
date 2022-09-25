8d9bf2cd39e254051f44da64c7cb7a72bfb88db7
prasbeck <ch20b083@smail.iitm.ac.in>
2022-09-26 02:56:13 +0530

    React Animation Components

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -5,7 +5,7 @@ import {
     Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle
 } from 'reactstrap';
-
+import { FadeTransform } from 'react-animation-components';
 function RenderCard({ item, isLoading, errMess }) {
 
     if (isLoading) {
@@ -21,14 +21,19 @@ function RenderCard({ item, isLoading, errMess }) {
 
     else
         return (
-            <Card>
-                <CardImg src={baseUrl + item.image} alt={item.name} />
-                <CardBody>
-                    <CardTitle>{item.name}</CardTitle>
-                    {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
-                    <CardText>{item.description}</CardText>
-                </CardBody>
-            </Card>
+            <FadeTransform in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                <Card>
+                    <CardImg src={baseUrl + item.image} alt={item.name} />
+                    <CardBody>
+                        <CardTitle>{item.name}</CardTitle>
+                        {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+                        <CardText>{item.description}</CardText>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
         );
 
 }
